a80770447ea6b45f5f88d0dc701f89b789dd3f5d
cso4tb <cso4tb@virginia.edu>
2014-11-11 19:11:54 -0800

    Update urls.py

--- sprint1/urls.py
+++ sprint1/urls.py
@@ -4,4 +4,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('sprint1.views',
     url(r'^list', 'list', name='list'),
     url(r'^', 'home',name='home')
-)
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.login, name='login'),
+    )
