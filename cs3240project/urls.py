0c5dfbb05ce4b862ae73a198ed9b5dc4f56deede
cso4tb <cso4tb@virginia.edu>
2014-11-16 18:21:20 -0800

    Update urls.py

--- cs3240project/urls.py
+++ cs3240project/urls.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+import contacts.views
+
 #from django.views.generic import RedirectView
 
 urlpatterns = patterns('',
