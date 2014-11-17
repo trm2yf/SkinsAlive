2346eefd920f4867632b3836e07932d8efc35ce5
cso4tb <cso4tb@virginia.edu>
2014-11-17 06:18:09 -0800

    Update settings.py

--- cs3240project/settings.py
+++ cs3240project/settings.py
@@ -80,7 +80,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATICFILES_DIRS = (
-    "C:\Users\student\Documents\GitHub\sw\cs3240-f14-team02\dist\",
+    "C:\Users\student\Documents\GitHub\sw\cs3240-f14-team02\dist\"
     
 
 
