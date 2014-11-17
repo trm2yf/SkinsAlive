93e74973035de74619ffd7381b6da152236cd416
zws3mb <zws3mb@virginia.edu>
2014-11-17 15:18:31 -0500

    Working folders and bulletin submission

--- sprint1/urls.py
+++ sprint1/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('sprint1.views',
     url(r'^register/$', 'register', name='register'),
     url(r'^login/$', 'user_login', name='login'),
     url(r'^profile', 'profile', name='profile'),
-    url(r'^logout', 'logout', name='logout')
+    url(r'^logout', 'logout', name='logout'),
+    url(r'^search', 'search', name='search')
     )
