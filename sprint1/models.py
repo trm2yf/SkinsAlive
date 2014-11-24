f7fefc4ed0b846790c627a253f925240ce3ec19f
Travis Moore <trm2yf@virginia.edu>
2014-11-24 15:04:56 -0500

    deleted duplicates in views

--- sprint1/models.py
+++ sprint1/models.py
@@ -44,45 +44,90 @@ class Bulletin(models.Model):
     def __str__(self):              # __unicode__ on Python 2
         return self.title
 # Create your models here.
-# from Crypto import Random
-# from Crypto.Cipher import AES
-#
-# def pad(s):
-#     return s + b"\0" * (AES.block_size - len(s) % AES.block_size)
-#
-# def encrypt(message, key, key_size=256):
-#     if key is None:
-#         key = Random.new().read(key_size // 8)
-#     message = pad(message)
-#     iv = Random.new().read(AES.block_size)
-#     cipher = AES.new(key, AES.MODE_CBC, iv)
-#     return iv + cipher.encrypt(message)
-#
-# def decrypt(ciphertext, key):
-#     iv = ciphertext[:AES.block_size]
-#     cipher = AES.new(key, AES.MODE_CBC, iv)
-#     plaintext = cipher.decrypt(ciphertext[AES.block_size:])
-#     return plaintext.rstrip(b"\0")
-#
-# def encrypt_file(file_name, key):
-#     print 'Encrypt called'
-#     with file_name as fo:
-#         plaintext = fo.read()
-#     enc = encrypt(plaintext, key)
-#     # fo=open('E'+file_name.name.split("/")[-1], 'wb')
-#     # fo.write(enc)
-#     # fo.close()
-#     return enc
-#
-# def decrypt_file(file_name, key):
-#     with open(file_name, 'rb') as fo:
-#         ciphertext = fo.read()
-#     dec = decrypt(ciphertext, key)
-#     with open(file_name[:-4], 'wb') as fo:
-#         fo.write(dec)
-#
-# def filepath_handler(instance,name):
-#     return path.join('user_%d'%instance.posted_bulletin.author.id,'bulletin_%d'%instance.posted_bulletin.b_key,name)
+
+from Crypto import Random
+from Crypto.Cipher import AES
+
+def pad(s):
+    return s + b"\0" * (AES.block_size - len(s) % AES.block_size)
+
+def encrypt(message, key, key_size=256):
+    if key is None:
+        key = Random.new().read(key_size // 8)
+    message = pad(message)
+    iv = Random.new().read(AES.block_size)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    return iv + cipher.encrypt(message)
+
+def decrypt(ciphertext, key):
+    iv = ciphertext[:AES.block_size]
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    plaintext = cipher.decrypt(ciphertext[AES.block_size:])
+    return plaintext.rstrip(b"\0")
+
+def encrypt_file(file_name, key):
+    print 'Encrypt called'
+    with open(file_name,'r') as fo:
+        plaintext = fo.read()
+    enc = encrypt(plaintext, key)
+    #file_name.close()
+    overwrite=open(file_name,'wb')
+    overwrite.write(enc)
+    overwrite.close()
+    # fo=open('E'+file_name.name.split("/")[-1], 'wb')
+    # fo.write(enc)
+    # fo.close()
+
+def decrypt_file(file_name, key):
+    with open(file_name, 'rb') as fo:
+        ciphertext = fo.read()
+    dec = decrypt(ciphertext, key)
+    with open(file_name[:-4], 'wb') as fo:
+        fo.write(dec)
+
+def filepath_handler(instance,name):
+    return path.join('user_%d'%instance.posted_bulletin.author.id,'bulletin_%d'%instance.posted_bulletin.b_key,name)
+
+from Crypto import Random
+from Crypto.Cipher import AES
+
+def pad(s):
+    return s + b"\0" * (AES.block_size - len(s) % AES.block_size)
+
+def encrypt(message, key, key_size=256):
+    if key is None:
+        key = Random.new().read(key_size // 8)
+    message = pad(message)
+    iv = Random.new().read(AES.block_size)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    return iv + cipher.encrypt(message)
+
+def decrypt(ciphertext, key):
+    iv = ciphertext[:AES.block_size]
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    plaintext = cipher.decrypt(ciphertext[AES.block_size:])
+    return plaintext.rstrip(b"\0")
+
+def encrypt_file(file_name, key):
+    print 'Encrypt called'
+    with file_name as fo:
+        plaintext = fo.read()
+    enc = encrypt(plaintext, key)
+    # fo=open('E'+file_name.name.split("/")[-1], 'wb')
+    # fo.write(enc)
+    # fo.close()
+    return enc
+
+def decrypt_file(file_name, key):
+    with open(file_name, 'rb') as fo:
+        ciphertext = fo.read()
+    dec = decrypt(ciphertext, key)
+    with open(file_name[:-4], 'wb') as fo:
+        fo.write(dec)
+
+def filepath_handler(instance,name):
+    return path.join('user_%d'%instance.posted_bulletin.author.id,'bulletin_%d'%instance.posted_bulletin.b_key,name)
+
 
 
 # Document Model
@@ -90,24 +135,28 @@ class Document(models.Model):
     posted_bulletin=models.ForeignKey(Bulletin)
   #  docfile = models.FileField(upload_to=filepath_handler)
     d_key = models.AutoField(primary_key=True)
-    # def save(self, *args, **kwargs):
-    #     print 'save called'
-    #     super(Document, self).save(*args, **kwargs)
-    #     if self.d_key:
-    #
-    #         #print self.docfile
-    #         enc=encrypt_file(self.docfile,None)
-    #         snake=path.join(getcwd(),'media',self.docfile.name)
-    #         with open(path.join(getcwd(),'media',self.docfile.name),'w') as f:
-    #             f.write(enc)
-    #         if not self.docfile:
-    #             self.docfile.save(self.docfile.name,ContentFile(open(snake)),save=False)
-            #temp=ContentFile(temp.read())
-            #self.docfile.save(self.docfile.name.split("/")[-1],content=ContentFile(StringIO.StringIO(self.docfile._file)),save=False)
-            #return
-            #self.docfile.save(self.docfile.name,encrypt(self.docfile.))
-            #... unzip your file ...
-            #... encrypt your file if necessary ...
+    def save(self, *args, **kwargs):
+        super(Document, self).save(*args, **kwargs)
+        #print self.docfile.name
+        snake=path.join(getcwd(),'media',self.docfile.name)
+        encrypt_file(snake,None)
+        #self.docfile=ContentFile.open(ContentFile(self.docfile),)
+
+        # print 'save called'
+        # super(Document, self).save(*args, **kwargs)
+        # print self.docfile
+        # name=self.docfile.file.name
+        # print name
+        # snake=path.join(getcwd(),'media',name)
+        # enc=encrypt_file(ContentFile(open(snake)),None)
+        #
+        # with open(path.join(getcwd(),'media',name),'w') as f:
+        #     f.write(enc)
+        # if not self.docfile:
+        #     self.docfile.save(name=name,content=ContentFile(open(snake)), save=False)
+        #     #self.docfile.close()
+        #     #self.docfile=ContentFile(open(snake))
+        # print 'Done'
 
 
 
