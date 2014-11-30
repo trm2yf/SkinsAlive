642e28ac0b470e063cdb37a8dafadccbcd80438a
cso4tb <cso4tb@virginia.edu>
2014-11-29 22:01:12 -0500

    Update forms.py

--- sprint1/forms.py
+++ sprint1/forms.py
@@ -16,7 +16,7 @@ class FolderForm(forms.ModelForm):
     # text_description=forms.CharField(label='Text Description')
     class Meta:
         model=Folder
-        fields=['name','text_description']
+        fields=['name']
     #def __init__(self):
       #       self.fields['folder'].initial=1
     """
