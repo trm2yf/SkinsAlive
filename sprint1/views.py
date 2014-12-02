3cc61bf849e137cffa2ec69abd857dacd3733ed8
zws3mb <zws3mb@virginia.edu>
2014-12-02 13:03:45 -0500

    working file decryption/encryption

--- sprint1/views.py
+++ sprint1/views.py
@@ -144,7 +144,7 @@ from Crypto import Random
 from Crypto.Hash import MD5
 
 # Use a larger key length in practice...
-KEY_LENGTH = 1024  # Key size (in bits)
+KEY_LENGTH = 2048  # Key size (in bits)
 random_gen = Random.new().read
 
 # Generate RSA private/public key pairs for both parties...
@@ -165,10 +165,12 @@ def register(request):
             user.set_password(user.password) #Django does this to password fields by default.
             user.save()
             pubkey=RSA.generate(KEY_LENGTH,random_gen)
-            key = Key(owner=user,public=pubkey.publickey().exportKey('PEM'))
+            pubstring=pubkey.publickey()
+            print len(str(pubstring.n))
+            pubstring=pubstring.exportKey('PEM')
+            key = Key(owner=user,public=pubstring)
             key.save()
             pkey=pubkey.exportKey('PEM')
-
 
             from django.core.mail import send_mail,EmailMessage
             mail = EmailMessage('SecureWitness', 'Do not lose the enclosed file. Do not reply.', ('Secure Witness','3240project@gmail.com'), (user.username,user.email))
@@ -332,6 +334,7 @@ def decrypt(request):
         from models import decrypt_file
         bcontents=decrypt_file(reqdoc,pkey.read())
         response=HttpResponse(content_type='multipart/encrypted')
+        response['Content-Disposition'] = 'attachment; filename='+reqdoc.split('/')[-1]
         response.write(bcontents)
         return response
     except Exception as e:
