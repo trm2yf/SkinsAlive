8ff7d448612b339ee57f4a365c4700b5bd77b45e
cso4tb <cso4tb@virginia.edu>
2014-12-03 10:50:24 -0500

    Update forms.py

--- sprint1/forms.py
+++ sprint1/forms.py
@@ -80,7 +80,7 @@ class AccountForm(forms.Form):
     
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
-    author = forms.BooleanField()
+    author = forms.BooleanField(required=False)
     #Type = forms.ChoiceField(choices=('Author','Reader')) 
 
     #nested Meta class
