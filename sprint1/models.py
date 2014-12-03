f5bcd244b84aecac62cd08ab4eec5ef095706c00
cso4tb <cso4tb@virginia.edu>
2014-12-02 21:41:53 -0500

    Update models.py

--- sprint1/models.py
+++ sprint1/models.py
@@ -34,7 +34,7 @@ class Bulletin(models.Model):
     text_description = models.TextField(max_length=1024)
     date_created = models.DateField(editable=False, default=datetime.datetime.today())
     date_modified = models.DateTimeField(editable=False, default=datetime.datetime.today())
-
+    num_views = models.IntegerField(max_digits=10)
     author= models.ForeignKey(User)
     lat = models.DecimalField(decimal_places=2,max_digits=10)
     long = models.DecimalField(decimal_places=2,max_digits=10)
