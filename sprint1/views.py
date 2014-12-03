8d5367e7e935f87fd4b5c6ff3ebf9ba4812d2fb4
cso4tb <cso4tb@virginia.edu>
2014-12-03 00:16:03 -0500

    Update views.py
    redirecting readers to frontpage.html and redirecting author's to their profile pages

--- sprint1/views.py
+++ sprint1/views.py
@@ -127,7 +127,10 @@ def user_login(request):
             #check if the account is active and then redirect back to main page
             if user.is_active:
                 login(request, user)
-                return HttpResponseRedirect('/index/')
+                if user.get_profile().author:
+                    return HttpResponseRedirect('/profile/')
+                else:
+                    return HttpResponseRedirect('/frontpage/')
             else:
                 #otherwise account is inactive
                 return HttpResponse("Account is not active")
