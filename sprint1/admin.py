d05d1dc3e87b9b8001b2d8903f29e17409db0112
cso4tb <cso4tb@virginia.edu>
2014-12-03 22:14:40 -0500

    Update admin.py

--- sprint1/admin.py
+++ sprint1/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from sprint1.models import Document, Bulletin, Folder
+from sprint1.models import Document, Bulletin, Folder, Author
 
 admin.site.register(Document)
 admin.site.register(Bulletin)
